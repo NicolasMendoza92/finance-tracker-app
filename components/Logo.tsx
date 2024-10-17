d898e4cee5a7b6ef6f3ed4a89fe8be5d935a7634
NicoMen <nicomendoza.92@gmail.com>
2024-10-17 21:22:58 +0200

    change styles and try to fix datepicker

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -16,7 +16,7 @@ function Logo() {
 export function LogoMobile() {
   return (
     <a href='/' className='flex items-center gap-2'>
-        <p className='bg-blue-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+        <p className='bg-blue-500 bg-clip-text text-xl font-bold leading-tight tracking-tighter text-transparent'>
             Entiende tus finazas 
         </p>
     </a>
